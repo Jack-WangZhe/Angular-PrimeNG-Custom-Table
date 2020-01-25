9ac31fd067030fa94db967d31e49ef887c7d467c
Jack <jack.zhe.wang@kingland.com>
2020-01-26 00:35:20 +0800

    更新table引入

--- src/app/components/dhrm-table/dhrm-table.component.ts
+++ src/app/components/dhrm-table/dhrm-table.component.ts
@@ -1,24 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'dhrm-table',
   templateUrl: './dhrm-table.component.html',
-  styleUrls: ['./dhrm-table.component.scss']
+  styleUrls: ['./dhrm-table.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class DhrmTableComponent implements OnInit {
 
-  cars: any[];
+  items: any[];
+  cols: any[];
+  selectedColumns: any[];
+  pageInfo = {
+    pageIndex: 0,
+    pageSize: 10,
+    totalCount: 18
+  }
+
+  actions = [
+    { iconName: 'fas fa-eye', title: 'view', type: 'view' },
+    { iconName: 'fas fa-edit', title: 'edit', type: 'edit' },
+    { iconName: 'fas fa-trash', title: 'delete', type: 'delete' }
+  ];
 
   constructor() { }
 
   ngOnInit() {
-      this.cars = [
+      this.items = [
         {'vin': 1, 'year': 1, 'brand': 1, 'color': 'red'},
         {'vin': 2, 'year': 2, 'brand': 2, 'color': 'red'},
         {'vin': 1, 'year': 1, 'brand': 1, 'color': 'red'},
         {'vin': 1, 'year': 1, 'brand': 1, 'color': 'red'},
         {'vin': 1, 'year': 1, 'brand': 1, 'color': 'red'}
       ];
+      this.cols = [
+        { field: 'vin', header: 'Vin' },
+        { field: 'year', header: 'Year' },
+        { field: 'brand', header: 'Brand' },
+        { field: 'color', header: 'Color' }
+      ];
+      this.selectedColumns = this.cols;
+  }
+
+  /**
+   * Action item click
+   * 
+   * @param type action type
+   * @param row row data
+   */
+  onActionClick(type: string, row: any) {
+    // TODO
+  }
+
+  customSort(event) {
+    console.log(event)
   }
 
 }
--- src/app/components/dhrm-table/dhrm-table.module.ts
+++ src/app/components/dhrm-table/dhrm-table.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DhrmTableComponent } from './dhrm-table.component';
 import { TableModule } from 'primeng/table';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PaginatorModule } from 'primeng/paginator';
 
 @NgModule({
   declarations: [DhrmTableComponent],
   imports: [
     CommonModule,
-    TableModule
+    FormsModule,
+    BrowserAnimationsModule,
+    TableModule,
+    MultiSelectModule,
+    PaginatorModule
   ],
   exports: [DhrmTableComponent]
 })
